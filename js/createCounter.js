6da09261b513adb8ad2f62dedaab7ccc71aeee3b
cedvid <cedricvidot@gmail.com>
2023-06-03 15:16:07 +0200

    add problem

---
+++ js/createCounter.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    var counter = n;
+    return function() {
+        var res = counter;
+        counter++;
+        return res;
+    };
+};
+
+
+// const counter = createCounter(10)
+// console.log(counter()) // 10
+// console.log(counter()) // 11
+// console.log(counter()) // 12
+
+
+// Given an integer n, return a counter function. This counter function initially returns n and then 
+// returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
